398278356bcf8e79ebec284a2c60837ea331bbd8
AndrePedro51 <andrepedro51@gmail.com>
2022-07-04 20:54:52 -0300

    Iniciando com Docker e TypeORM

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 
 app.use(express.json());
